475de5d0d36c7ef04c9b3f80f65077f65a64db0a
Omenka-da-boss <omenkandubuisi@gmail.com>
2026-04-07 01:23:12 +0100

    updated

--- src/utils/validate.py
+++ src/utils/validate.py
@@ -2,7 +2,7 @@ import great_expectations as gx
 import pandas as pd
 
 # Load dataset
-# df = pd.read_csv("mlops\\EDA\\lung_cancer_dataset.csv")
+df = pd.read_csv("mlops\\EDA\\lung_cancer_dataset.csv")
 
 def validate_data(df):
     context = gx.get_context()
@@ -110,3 +110,6 @@ def validate_data(df):
 
         # print(results["success"], failed_expectations)
         return results["success"], failed_expectations
+    
+    
+validate_data(df)
